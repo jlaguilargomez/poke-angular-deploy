46818916a2d734ed9c8eb5e89b4683e47c1f2a10
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-03 14:11:25 +0100

    map works but pokemon doesn't appear on it

--- src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
+++ src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
@@ -9,7 +9,7 @@ import { PokedexService } from 'src/app/pokedex.service';
 	templateUrl: './pokemon-map.component.html',
 	styleUrls: ['./pokemon-map.component.scss'],
 })
-export class PokemonMapComponent implements OnInit {
+export class PokemonMapComponent implements OnInit, DoCheck {
 	map: any;
 	pokemons: Pokemon[];
 	selectedPokemon = 0;
@@ -32,6 +32,10 @@ export class PokemonMapComponent implements OnInit {
 		);
 	}
 
+	ngDoCheck() {
+		console.log('something has happend!');
+	}
+
 	ngOnInit() {
 		console.log(
 			'pokemon-map component works!, it must display in the console (...) the pokemon clicked on the list'
@@ -47,54 +51,78 @@ export class PokemonMapComponent implements OnInit {
 			console.log(this.pokemonSelected);
 		});
 
+		// var mymap = L.map('map').setView([51.505, -0.09], 13);
+		// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+		// 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		// 	maxZoom: 18,
+		// 	id: 'mapbox/streets-v11',
+		// 	accessToken: 'your.mapbox.access.token'
+		// }).addTo(mymap);
+
+		const options = { attributionControl: false };
+		const map = L.map('map', options).setView(
+			[this.pokemonSelected.coord.lat, this.pokemonSelected.coord.long],
+			13
+		);
+		const tileLayer = L.tileLayer(
+			'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+			{
+				attribution: '[...] GIS User Community',
+				/* 'Tiles &copy; Esri &mdash; 
+		Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, 
+		Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',*/
+			}
+		);
+		tileLayer.addTo(map);
+
 		// this used to work:
-		this.pokedexService.pokemonsLoaded.subscribe((pokemons: Pokemon[]) => {
-			this.pokemons = pokemons;
-			// Leaflet setting
-			const options = { attributionControl: false };
-			const map = L.map('map', options).setView(
-				[
-					pokemons[this.selectedPokemon].coord.lat,
-					pokemons[this.selectedPokemon].coord.long,
-				],
-				13
-			);
-			const tileLayer = L.tileLayer(
-				'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-				{
-					attribution: '[...] GIS User Community',
-					/* 'Tiles &copy; Esri &mdash; 
-            Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, 
-            Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',*/
-				}
-			);
-			tileLayer.addTo(map);
+		// this.pokedexService.pokemonsLoaded.subscribe((pokemons: Pokemon[]) => {
+		// 	this.pokemons = pokemons;
+		// 	// Leaflet setting
+		// 	const options = { attributionControl: false };
+		// 	const map = L.map('map', options).setView(
+		// 		[
+		// 			pokemons[this.selectedPokemon].coord.lat,
+		// 			pokemons[this.selectedPokemon].coord.long,
+		// 		],
+		// 		13
+		// 	);
+		// 	const tileLayer = L.tileLayer(
+		// 		'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+		// 		{
+		// 			attribution: '[...] GIS User Community',
+		// 			/* 'Tiles &copy; Esri &mdash;
+		//     Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping,
+		//     Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',*/
+		// 		}
+		// 	);
+		// 	tileLayer.addTo(map);
 
-			pokemons.forEach(pokemon => {
-				const pokemonIcon = L.icon({
-					iconUrl: pokemon.imagePath,
-					iconSize: 70,
-					shadowUrl: '../../../assets/img/iconShadow.png',
-					shadowSize: 70,
-					popupAnchor: [0, -35],
-				});
-				const marker = L.marker([pokemon.coord.lat, pokemon.coord.long], {
-					icon: pokemonIcon,
-				}).bindPopup(pokemon.name, { closeButton: false });
-				marker.on('mouseover', () => {
-					marker.openPopup();
-				});
-				marker.on('click', event => {
-					// Extact the pokemon number using the imagePath
-					console.log(event.target._icon.src.match(/\d+/)[0]);
-				});
-				marker.on('mouseout', () => {
-					marker.closePopup();
-				});
-				marker.addTo(map);
-			});
+		// 	pokemons.forEach(pokemon => {
+		// 		const pokemonIcon = L.icon({
+		// 			iconUrl: pokemon.imagePath,
+		// 			iconSize: 70,
+		// 			shadowUrl: '../../../assets/img/iconShadow.png',
+		// 			shadowSize: 70,
+		// 			popupAnchor: [0, -35],
+		// 		});
+		// 		const marker = L.marker([pokemon.coord.lat, pokemon.coord.long], {
+		// 			icon: pokemonIcon,
+		// 		}).bindPopup(pokemon.name, { closeButton: false });
+		// 		marker.on('mouseover', () => {
+		// 			marker.openPopup();
+		// 		});
+		// 		marker.on('click', event => {
+		// 			// Extact the pokemon number using the imagePath
+		// 			console.log(event.target._icon.src.match(/\d+/)[0]);
+		// 		});
+		// 		marker.on('mouseout', () => {
+		// 			marker.closePopup();
+		// 		});
+		// 		marker.addTo(map);
+		// 	});
 
-			this.map = map;
-		});
+		// 	this.map = map;
+		// });
 	}
 }
