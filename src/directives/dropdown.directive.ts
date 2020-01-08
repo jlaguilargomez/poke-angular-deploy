3b4b3d9923ebe14a7e3c4cff84cf09e065abc1d8
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-08 15:40:47 +0100

    basic configuration of the dropdown.directive file

---
+++ src/directives/dropdown.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+// this directive allows to change the class of the dropdown
+
+@Directive({
+	selector: '[appDropdown]',
+})
+export class DropDownDirective {}
