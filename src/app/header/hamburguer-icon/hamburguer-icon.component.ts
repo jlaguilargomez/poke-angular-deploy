ea6bc46906837697fc5b2f924470957d88e28b15
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-08 13:04:50 +0100

    hamburguer menu (static) added to 480 px

--- src/app/header/hamburguer-icon/hamburguer-icon.component.ts
+++ src/app/header/hamburguer-icon/hamburguer-icon.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
 	selector: 'app-hamburguer-icon',
@@ -8,5 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class HamburguerIconComponent implements OnInit {
 	constructor() {}
 
-	ngOnInit() {}
+	icon: HTMLElement;
+	menu: HTMLElement;
+
+	renderSomething(e) {
+		console.log(e);
+	}
+
+	ngOnInit() {
+		this.icon = document.querySelector('.hamburguer-icon');
+		this.menu = document.querySelector('.hamburguer-icon');
+
+		console.log(this.icon);
+
+		fromEvent(this.icon, 'click').subscribe(() => console.log('clicked'));
+	}
 }
