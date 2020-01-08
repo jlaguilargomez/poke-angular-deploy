b55cd015abc2beed8224f3a14c039a09b9310acb
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-08 11:32:32 +0100

    added new component: hamburguer-icon

---
+++ src/app/header/hamburguer-icon/hamburguer-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-hamburguer-icon',
+	templateUrl: './hamburguer-icon.component.html',
+	styleUrls: ['./hamburguer-icon.component.scss'],
+})
+export class HamburguerIconComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
