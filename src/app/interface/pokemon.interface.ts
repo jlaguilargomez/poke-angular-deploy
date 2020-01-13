eac162eb0f270d64639b2694329ae59f8c7b81ce
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-01-13 12:43:38 +0100

    Test for a correct load of pokemonList works

--- src/app/interface/pokemon.interface.ts
+++ src/app/interface/pokemon.interface.ts
@@ -10,3 +10,14 @@ export interface Pokemon {
 	imagePath: string;
 	coord: { lat: number; long: number };
 }
+
+export interface PokemonTest {
+	id: number;
+	name: string;
+	type: string[];
+	height: number;
+	weight: number;
+	stats: { name: string; base: number }[];
+	ability: string[];
+	imagePath: string;
+}
--- src/app/pokedex/pokedex.service.spec.ts
+++ src/app/pokedex/pokedex.service.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed, inject, async } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { of, Observable } from 'rxjs';
+import { Pokemon, PokemonTest } from '../interface/pokemon.interface';
 import { PokedexService } from './pokedex.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -23,7 +24,65 @@ describe('Service: PokedexService', () => {
 		expect(pokedexService).toBeTruthy();
 	});
 
-	// it('the service works correctly', () => {
-	//   expect(pokedexService.getPokemons(151)).toBeTruthy();
-	// });
+	// Add test for loadPokemonList() method
+	describe('loadPokemonList correctly', () => {
+		beforeEach(done => {
+			pokedexService.loadPokemonList(1, 151).subscribe(() => {
+				done();
+			});
+		});
+
+		it('should get the pokemonList', () => {
+			let response: Pokemon[] = pokedexService.getPokemonList();
+			expect(response.length).toBe(151);
+		});
+
+		it('should get an specific pokemon', () => {
+			const getPokemonResponse: PokemonTest = {
+				id: 1,
+				name: 'Bulbasaur',
+				type: ['poison', 'grass'],
+				height: 7,
+				weight: 69,
+				stats: [
+					{
+						name: 'speed',
+						base: 45,
+					},
+					{
+						name: 'special-defense',
+						base: 65,
+					},
+					{
+						name: 'special-attack',
+						base: 65,
+					},
+					{
+						name: 'defense',
+						base: 49,
+					},
+					{
+						name: 'attack',
+						base: 49,
+					},
+					{
+						name: 'hp',
+						base: 45,
+					},
+				],
+				ability: ['chlorophyll', 'overgrow'],
+				imagePath:
+					'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+			};
+			let response: any = pokedexService.getPokemon(0);
+			let err: any = pokedexService.getPokemon(-1);
+
+			// Remove the parameters created with Math.random variable
+			delete response.moves;
+			delete response.coord;
+
+			expect(response).toEqual(getPokemonResponse);
+			expect(err).toBeUndefined();
+		});
+	});
 });
