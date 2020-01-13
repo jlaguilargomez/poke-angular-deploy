0204938b4c949b77eb2946345a94f3c23a15a01b
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-01-13 10:18:50 +0100

    minor bug in interface was found

--- src/app/interface/pokemon.interface.ts
+++ src/app/interface/pokemon.interface.ts
@@ -1,12 +1,12 @@
 export interface Pokemon {
-  id: number;
-  name: string;
-  type: string[];
-  height: number;
-  weight: number;
-  stats: [{ name: string; 'base-stat': number }];
-  moves: string[];
-  ability: string[];
-  imagePath: string;
-  coord: { lat: number; long: number };
+	id: number;
+	name: string;
+	type: string[];
+	height: number;
+	weight: number;
+	stats: { name: string; base: number }[];
+	moves: string[];
+	ability: string[];
+	imagePath: string;
+	coord: { lat: number; long: number };
 }
