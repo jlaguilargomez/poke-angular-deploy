db692d339fbae43da05f963abe6eb5b8ae0388d6
Jose Angel Civico Martos <jacivico@minsait.com>
2020-01-14 09:18:45 +0100

    Add 7-1 Arquitecture configuration

--- src/app/footer/footer.component.spect.ts
+++ src/app/footer/footer.component.spect.ts
@@ -2,16 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 
-describe('FooterComponent', () => {
-	beforeEach(() => {
-		TestBed.configureTestingModule({
-			declarations: [FooterComponent],
-		}).compileComponents();
-	});
+// describe('FooterComponent', () => {
+// 	beforeEach(() => {
+// 		TestBed.configureTestingModule({
+// 			declarations: [FooterComponent],
+// 		}).compileComponents();
+// 	});
 
-	it('Component Footer works!', () => {
-		const fixture = TestBed.createComponent(FooterComponent);
-		const component = fixture.debugElement.componentInstance;
-		expect(component).toBeTruthy();
-	});
-});
+// 	it('Component Footer works!', () => {
+// 		const fixture = TestBed.createComponent(FooterComponent);
+// 		const component = fixture.debugElement.componentInstance;
+// 		expect(component).toBeTruthy();
+// 	});
+// });
