c9092ffa1702aa4166f2208ef0ab846a62d21dcb
Jose Angel Civico Martos <jacivico@minsait.com>
2020-01-14 16:23:49 +0100

    Add new component of template placeholder decoration style in functionality

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonArrowComponent } from './button-arrow/button-arrow.component';
+import { BackgroundDexStyleComponent } from '../background-dex-style/background-dex-style.component';
 
 @NgModule({
 	imports: [],
-	exports: [ButtonArrowComponent],
-	declarations: [ButtonArrowComponent],
+	exports: [ButtonArrowComponent, BackgroundDexStyleComponent],
+	declarations: [ButtonArrowComponent, BackgroundDexStyleComponent],
 	providers: [],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
