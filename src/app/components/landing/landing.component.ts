16fb67c727f21c1c925f676b39823d57be142413
Jose Angel Civico Martos <jacivico@minsait.com>
2020-01-16 10:45:42 +0100

    Serialization of functionality data in landing.component.ts

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/models/member.interface';
 import { Functionality } from 'src/app/models/functionality.interface';
+import { LandingService } from 'src/app/services/landing.service';
 
 @Component({
 	selector: 'app-landing',
@@ -9,61 +10,15 @@ import { Functionality } from 'src/app/models/functionality.interface';
 })
 export class LandingComponent implements OnInit {
 	pokemons: string[] = [];
+	team: Member[] = [];
+	functionalities: Functionality[] = [];
 	currentPokemon: string;
 	oldPokemon: string;
 	imgSource: string;
-	team: Member[] = [
-		{
-			name: 'Jose Angel Cívico Martos',
-			img: '../../assets/img/icon-profile1.png',
-			job: 'Software Developer',
-		},
-		{
-			name: 'Gabriel García Rodríguez',
-			img: '../../assets/img/icon-profile2.png',
-			job: 'Software Developer',
-		},
-		{
-			name: 'Guadalupe García Plaza',
-			img: '../../assets/img/icon-profile3.png',
-			job: 'Software Developer',
-		},
-		{
-			name: 'Jose Luis Aguilar Gómez',
-			img: '../../assets/img/icon-profile4.png',
-			job: 'Software Developer',
-		},
-		{
-			name: 'Juan Pablo Amador Díaz',
-			img: '../../assets/img/icon-profile5.png',
-			job: 'Software Developer',
-		},
-	];
-	functionalities: Functionality[] = [
-		{
-			num: '1',
-			title: 'Search your Pokemon',
-			img: 'assets/img/functionality-1.jpg',
-			info:
-				'You can select the first 151 Pokemons and search them by name',
-		},
-		{
-			num: '2',
-			title: 'Analyze your selected Pokemon',
-			img: 'assets/img/functionality-2_1.jpg',
-			info:
-				'Just point the cursor on the card and you will see all the data available: Name, ability, height, weight, types and stats!',
-		},
-		{
-			num: '3',
-			title: 'And catch&#8217;em all!',
-			img: 'assets/img/functionality-3.jpg',
-			info:
-				'Explore the world around you and catch all the Pokemon availables!',
-		},
-	];
 
-	constructor() {
+	constructor(private landingService: LandingService) {
+		this.team = this.landingService.getMembers();
+		this.functionalities = this.landingService.getFunctionalities();
 		this.pokemons = [
 			'bulbasaur',
 			'charmander',
