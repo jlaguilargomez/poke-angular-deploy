b76f5f2d5f4669d0a1956307ead38402ccc01bf2
Jose Angel Civico Martos <jacivico@minsait.com>
2020-01-16 09:35:16 +0100

    Add background-dex-style inside card-functionality component

--- src/app/components/landing/landing.component.spec.ts
+++ src/app/components/landing/landing.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
 import { ButtonArrowComponent } from 'src/app/shared/components/button-arrow/button-arrow.component';
-import { BackgroundDexStyleComponent } from 'src/app/shared/components/background-dex-style/background-dex-style.component';
+import { BackgroundDexStyleComponent } from 'src/app/shared/components/card-functionality/background-dex-style/background-dex-style.component';
 import { HeaderCurveComponent } from 'src/app/shared/components/header-curve/header-curve.component';
 import { CardMemberComponent } from 'src/app/shared/components/card-member/card-member.component';
 import { CardFunctionalityComponent } from 'src/app/shared/components/card-functionality/card-functionality.component';
--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonArrowComponent } from './button-arrow/button-arrow.component';
-import { BackgroundDexStyleComponent } from './background-dex-style/background-dex-style.component';
+import { BackgroundDexStyleComponent } from './card-functionality/background-dex-style/background-dex-style.component';
 import { HeaderCurveComponent } from './header-curve/header-curve.component';
 import { CardMemberComponent } from './card-member/card-member.component';
 import { CardFunctionalityComponent } from './card-functionality/card-functionality.component';
