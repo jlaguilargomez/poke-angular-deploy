1a86ffec0c3601423eff888688fe707ac4ec6ba4
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-01-16 09:55:10 +0100

    refactor pokemon-card completed. TODO: responsive for small devices

--- src/app/components/pokedex/pokemon-detail/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokedex/pokemon-detail/pokemon-card/pokemon-card.component.ts
@@ -19,6 +19,7 @@ export class PokemonCardComponent implements OnInit {
 
 	public ngOnInit(): void {
 		this.routerData.params.subscribe((params: Params) => {
+			this.detail = false;
 			this.pokemonSelected = this.pokedexService.getPokemon(params['id']);
 		});
 	}
@@ -26,6 +27,10 @@ export class PokemonCardComponent implements OnInit {
 	public colorBar(num: number): string {
 		const statusValue: number = num;
 		return `hsl(${statusValue}, 100%, 50%)`;
+	}
+
+	public transformBaseStat(base: number) {
+		return base / 1.5;
 	}
 
 	public get pokemonColorType(): string {
